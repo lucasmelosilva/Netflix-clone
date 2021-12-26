3a7ee69e368d296672f97067c4eb942adca6a71f
lucas melo <lucas.melo044@gmail.com>
2021-12-26 18:43:04 -0300

    add flexas de navegacao

--- src/components/MovieRow/MovieRow.jsx
+++ src/components/MovieRow/MovieRow.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import './MovieRow.css';
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 
 function MovieRow({ title, items }) {
 
   return (
     <div className="movieRow">
       <h2>{title}</h2>
+
+      <div className="movieRow--left">
+        <NavigateBeforeIcon style={{ fontSize: 50 }} />
+      </div>
+
+      <div className="movieRow--right">
+        <NavigateNextIcon style={{ fontSize: 50 }} />
+      </div>
+
       <div className="movieRow--listarea">
         <div className="movieRow--list">
           {items.results.map((item, key) => (
@@ -14,9 +25,9 @@ function MovieRow({ title, items }) {
               <img key={key} src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt="" />
             </div>
           ))}
-
         </div>
       </div>
+
     </div>
   );
 }
