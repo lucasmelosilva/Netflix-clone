897c407ed24b4eca48ef50582ae560b52c3b8107
lucas melo <lucas.melo044@gmail.com>
2021-12-26 15:28:53 -0300

    add estilizacao do header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,13 @@ function Header() {
     <header>
       <div className="header--logo">
         <a href="/">
-          <img src="https://en.wikipedia.org/wiki/Netflix#/media/File:Netflix_2015_logo.svg" alt="" />
+          <img src="https://logodownload.org/wp-content/uploads/2014/10/netflix-logo-5.png" alt="Netflix" />
+        </a>
+      </div>
+
+      <div className="header--user">
+        <a href="/">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="usuario" />
         </a>
       </div>
     </header>
