6451d8d7f7124f89f3e992b91b82e82941736775
lucas melo <lucas.melo044@gmail.com>
2021-12-26 19:21:54 -0300

    add funcao de scrolling em MovieList

--- src/components/MovieRow/MovieRow.jsx
+++ src/components/MovieRow/MovieRow.jsx
@@ -1,24 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './MovieRow.css';
 import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 
 function MovieRow({ title, items }) {
+  const halfWidthScreen = Math.round(window.innerWidth / 2);
+  const [scrollX, setScrollX] = useState(0);
+
+  function handleLeftArrow() {
+    let x = scrollX + halfWidthScreen;
+    if(x > 0){
+      x = 0;
+    }
+    setScrollX(x);
+  }
+
+  function handleRightArrow() {
+    let x = scrollX - halfWidthScreen;
+    let listWidth = items.results.length * 150;
+    if((window.innerWidth - listWidth) > x){
+      x = window.innerWidth - listWidth - 60;
+    }
+    setScrollX(x);
+  }
 
   return (
     <div className="movieRow">
       <h2>{title}</h2>
 
-      <div className="movieRow--left">
+      <div className="movieRow--left" onClick={handleLeftArrow}>
         <NavigateBeforeIcon style={{ fontSize: 50 }} />
       </div>
 
-      <div className="movieRow--right">
+      <div className="movieRow--right" onClick={handleRightArrow}>
         <NavigateNextIcon style={{ fontSize: 50 }} />
       </div>
 
       <div className="movieRow--listarea">
-        <div className="movieRow--list">
+        <div
+          className="movieRow--list"
+          style={{ 
+            marginLeft: scrollX,
+            width: items.results.length* 150 
+          }}
+        >
           {items.results.map((item, key) => (
 
             <div className="movieRow--item">
